bb1eec8e1b882906e13fa2687086e816fe438ad9
ricnah <ikciry@gmail.com>
2024-08-05 20:23:17 +0700

    Memperbarui Service

--- service/service.go
+++ service/service.go
@@ -17,11 +17,10 @@ import (
 func setRoutes(cfg *config.Config) (r *gin.Engine, err error) {
     r = gin.New()
 
-    // Inisialisasi repository dan usecase untuk user
+    
     userRepo := userrepo.UserCreateRepository(cfg.DB)
     userCase := usercase.UserCreateUsecase(userRepo)
 
-    // Inisialisasi repository dan usecase untuk produk
     productRepo := repository.NewProductRepository(cfg.DB)
     productUsecase := usecase.NewProductUsecase(productRepo)
     productHandler := handler.NewProductHandler(productUsecase)
@@ -29,26 +28,20 @@ func setRoutes(cfg *config.Config) (r *gin.Engine, err error) {
     r.Use(corsConfig())
     r.Use(middlewares.ActivityLogger())
 
-    // Group API routes under /api
     api := r.Group("/api")
     v1 := api.Group("/v1")
     {
-        // Rute untuk user
         userModule.InitRoutes(v1, userCase, cfg)
         
-        // Rute untuk produk
         product.InitRoutes(v1, productHandler) // Gunakan productHandler
     }
 
-    // Redirect root URL to /public
     r.GET("/", func(c *gin.Context) {
         c.Redirect(302, "/public")
     })
 
-    // Serve static files from the public directory with the updated prefix
     r.Static("/public", "./view/.output/public")
 
-    // Catch-all route to serve index.html for client-side routing
     r.NoRoute(func(c *gin.Context) {
         c.File("./view/.output/public/index.html")
     })
