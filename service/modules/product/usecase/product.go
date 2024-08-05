8ecf6115a54cc02d3bb37538e4b42beaf58941db
ricnah <ikciry@gmail.com>
2024-08-05 20:18:28 +0700

    Memperbarui models product

--- service/modules/product/usecase/product.go
+++ service/modules/product/usecase/product.go
@@ -16,41 +16,41 @@ func NewProductUsecase(repo repository.ProductRepository) ProductUsecase {
 }
 
 func (u *productUsecase) CreateProduct(ctx *gin.Context, product models.Product) (models.Product, terror.ErrInterface) {
-    err := u.productRepo.CreateProduct(&product)
-    if err != nil {
-        return models.Product{}, err 
+    terr := u.productRepo.CreateProduct(&product)
+    if terr != nil {
+        return models.Product{}, terr
     }
     return product, nil
 }
 
 func (u *productUsecase) GetProducts(ctx *gin.Context) ([]models.Product, terror.ErrInterface) {
-    products, err := u.productRepo.GetProducts()
-    if err != nil {
-        return nil, err 
+    products, terr := u.productRepo.GetProducts()
+    if terr != nil {
+        return nil, terr
     }
     return products, nil
 }
 
 func (u *productUsecase) GetProductByID(ctx *gin.Context, id int64) (models.Product, terror.ErrInterface) {
-    product, err := u.productRepo.GetProductByID(id)
-    if err != nil {
-        return models.Product{}, err 
+    product, terr := u.productRepo.GetProductByID(id)
+    if terr != nil {
+        return models.Product{}, terr
     }
     return product, nil
 }
 
 func (u *productUsecase) UpdateProduct(ctx *gin.Context, product models.Product) (models.Product, terror.ErrInterface) {
-    err := u.productRepo.UpdateProduct(&product)
-    if err != nil {
-        return models.Product{}, err 
+    terr := u.productRepo.UpdateProduct(&product)
+    if terr != nil {
+        return models.Product{}, terr
     }
     return product, nil
 }
 
-func (u *productUsecase) DeleteProduct(ctx *gin.Context, id int64) (terror.ErrInterface) {
-    err := u.productRepo.DeleteProduct(id)
-    if err != nil {
-        return err 
+func (u *productUsecase) DeleteProduct(ctx *gin.Context, id int64) terror.ErrInterface {
+    terr := u.productRepo.DeleteProduct(id)
+    if terr != nil {
+        return terr
     }
     return nil
 }
--- types/models/product.go
+++ types/models/product.go
@@ -1,17 +1,9 @@
 package models
 
-import (
-	"time"
-)
-
-// Product represents the structure of the product entity in the database
 type Product struct {
-	ID          uint       `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name        string     `json:"name"`
-	Description string     `json:"description"`
-	Price       float64    `json:"price"`
-	Quantity    float64    `json:"quantity"` // Update tipe data Quantity menjadi float64
-	CreatedAt   time.Time  `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time  `json:"updated_at" gorm:"autoUpdateTime"`
-	DeletedAt   *time.Time `json:"deleted_at,omitempty" gorm:"index"`
+	DbStandard
+	Name        string  `json:"name" binding:"required"`
+	Description string  `json:"description" binding:"required"`
+	Price       float64 `json:"price" binding:"required"`
+	Quantity    float64 `json:"quantity" binding:"required"`
 }
